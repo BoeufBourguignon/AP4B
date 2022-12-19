3a0f2f60da9c641332a6385937ad4f51f1d4c7e5
BoeufBourguignon <thibaud.leclere@gmail.com>
2022-12-19 19:02:38 +0100

    Implementation class Tile

--- src/azul/model/Tile.java
+++ src/azul/model/Tile.java
@@ -1,6 +1,32 @@
+/**
+ * @date 19/12/2022
+ * @brief Les Tile sont l'élément de base du jeu
+ * Elles peuvent être de différents types, définis par l'énumération TileType
+ */
+
 package azul.model;
 
 public class Tile 
 {
-
+	private TileType type;
+	
+	/**
+	 * Constructeur surchargé avec le type de la Tile
+	 * 
+	 * @param type Type de la tuile à créer
+	 */
+	public Tile(TileType type)
+	{
+		this.type = type;
+	}
+	
+	/**
+	 * Accesseur du type de la tuile
+	 * 
+	 * @return Elément de l'énumération TileType, correspondant au type de la tuile
+	 */
+	public TileType getType()
+	{
+		return this.type;
+	}
 }
