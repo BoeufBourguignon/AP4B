a4bdca56e8393b651616b494d103817251e7b2d0
LolaRoq <113534307+LolaRoq@users.noreply.github.com>
2022-12-30 11:04:41 +0100

    Update Disk.java
    add disk

--- src/azul/model/Disk.java
+++ src/azul/model/Disk.java
@@ -1,6 +1,38 @@
 package azul.model;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
 
-public class Disk
-{
+import static java.lang.Math.random;
 
+public class Disk {
+        
+        private List<Tile> disk;
+        private Deck deck;
+
+        public Disk()
+        {
+            for (int i=0; i<4; i++)
+            {
+                int rand = (int) Math.random()*(100);
+                if (deck.getDeck().get(rand).getType()!=TileType.First)
+                {
+                    disk.add(deck.getDeck().get(rand));
+                }
+            }
+        }
+
+        public List<Tile> find (TileType type)
+        {
+            List<Tile> TypeOndisk = null;
+            for (int i=0; i<disk.size(); i++)
+            {
+                if (disk.get(i).getType()==type)
+                {
+                    TypeOndisk.add(disk.get(i));
+                }
+            }
+            return TypeOndisk;
+        }
 }
+
