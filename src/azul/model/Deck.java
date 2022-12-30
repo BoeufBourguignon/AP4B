0976e5137ce0592b1b7265cae3ad257ff310bb66
LolaRoq <113534307+LolaRoq@users.noreply.github.com>
2022-12-30 11:05:21 +0100

    Update Deck.java
    Pas sur de tout

--- src/azul/model/Deck.java
+++ src/azul/model/Deck.java
@@ -1,6 +1,35 @@
 package azul.model;
 
-public class Deck
-{
-	
+import java.util.ArrayList;
+import java.util.List;
+
+import static azul.model.TileType.TCS;
+
+public class Deck {
+    private List<Tile> deck = new ArrayList<>(101);
+
+    public Deck()
+    {
+        for (int i=0; i<5; i++)
+        {
+            for (int j = 0; j < 20; j++)
+            {
+                Tile tile = new Tile(i);
+                deck.add(tile);
+            }
+        }
+        Tile tile = new Tile(6);
+        deck.add(tile);
+    }
+
+    public Tile drawTile()
+    {
+
+        return null;
+    }
+
+    public List<Tile> getDeck()
+    {
+        return deck;
+    }
 }
