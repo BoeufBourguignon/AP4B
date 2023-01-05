6754de5ec1936a65b30bcff3af51effa02dded3a
Guillaume SCHERRER <114092655+guiguiSCH68@users.noreply.github.com>
2023-01-05 11:34:57 +0100

    ajout Javadoc

--- src/azul/model/Deck.java
+++ src/azul/model/Deck.java
@@ -1,9 +1,17 @@
+package azul.model;
+
 import java.util.*;
-import java.lang.Math;
+
 public class Deck {
 
+    //-----Attributs-----
     private ArrayList<Tile> deck;
 
+    //-----Méthodes-----
+
+    /**
+     * Constructeur
+     */
     public Deck(){
 
         //----- création de la "liste" deck contenant 4 Tiles
@@ -49,10 +57,17 @@ public class Deck {
         }
     }
 
+    /**
+     * @return le deck sous forme de liste
+     */
     public List<Tile>getDeck(){
         return this.deck;
     }
 
+    /**
+     * choisis une Tile au hasard parmi celles disponibles
+     * @return la Tile choisie
+     */
     public Tile drawTile(){
 
         Random random = new Random();
@@ -60,15 +75,8 @@ public class Deck {
         int taille_deck = deck.size() - 1; // on fait "-1" vu qu'on compte sur les index
         Tile tile_a_retourner;
 
-        nombre = random.nextInt(100); //génère un nombre en 0 et 99
+        nombre = random.nextInt(taille_deck+1); //génère un nombre en 0 et le nombre de tiles dans le deck
 
-        if (nombre > taille_deck ){
-
-            tile_a_retourner = deck.get(taille_deck);
-            deck.remove(taille_deck); // on la supprime du deck vu qu'elle est utilisée
-
-            return tile_a_retourner;
-        }
         tile_a_retourner = deck.get(nombre); // on retourne la tile situé à cet index
         deck.remove(nombre); // on la supprime du deck vu qu'elle est utilisée
 
